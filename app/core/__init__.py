a0e09e616252b1538496d52ae8a3d8213084f708
Deshraj <deshrajdry@gmail.com>
2015-05-20 18:38:32 +0530

    rebuilt docs

--- app/core/__init__.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'dexter'
-# Path Hack
-import sys
-import os
-sys.path.insert(0, os.path.abspath('..'))
---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:46,filenames:["includeme","index"],objects:{},objnames:{},objtypes:{},terms:{cloud:0,comput:0,content:1,distribut:0,index:1,larg:0,modul:1,page:1,scale:0,search:1,servic:0,vision:0},titles:["CloudCV-Server","Welcome to CloudCV-Server&#8217;s documentation!"],titleterms:{cloudcv:[0,1],document:1,indic:1,server:[0,1],tabl:1,welcom:1}})
